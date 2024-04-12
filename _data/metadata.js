df98eeb5d33b09e326cb026d7138b23c051df9f6
Fintane <153891417+Fintanity@users.noreply.github.com>
2024-04-12 13:56:20 +0100

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,13 +1,13 @@
 // update the information in this file with information about you/your site - this is for the search engine to read and display, but isn't shown on your site itself
 
 module.exports = {
-	title: "My Example Blog",
-	url: "https://example.com/",
+	title: "A Basic Blog By Bishop",
+	url: "https://afintaneblog.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "My thoughts, my dreams, and again my thoughts",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Bishop Fintan",
+		email: "12FintanB@gmail.com",
+		url: "https://afintaneblog.com/about-me/"
 	}
 }
